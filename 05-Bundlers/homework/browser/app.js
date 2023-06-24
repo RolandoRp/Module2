b72d9b235014680ec73db3e58f857595fee2993e
RolandoRp <rolyrp@hotmail.com>
2023-06-23 18:18:31 -0600

    mod2hw5

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,6 +1,7 @@
-(function () {
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+const whiteboard = require('./whiteboard');
+const io = require('socket.io-client');
+
+  var socket = io(window.location.origin);
 
   socket.on("connect", function () {
     console.log("Connected!");
@@ -21,5 +22,5 @@
 
   whiteboard.on("draw", function (start, end, color) {
     socket.emit("draw", start, end, color);
-  });
-})();
+  });//14:25
+
