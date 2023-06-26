01d48fb5331e3d44e2c698d739e25e78ece9f119
RolandoRp <rolyrp@hotmail.com>
2023-06-26 17:05:19 -0600

    mod2hw1

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Kitane";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Aqui puedo escribir lo que quiera</h1>
+      <h3>{ studentName }</h3>
+      <ul>
+        {techSkills.map((skill) => {
+          return <li key = {skill}>{skill}</li>
+        })}
+      </ul>
+      <Botones alerts = {alerts}/>
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
